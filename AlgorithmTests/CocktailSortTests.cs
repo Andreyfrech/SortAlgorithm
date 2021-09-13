c77d717443b278a7497830f85c888297369a9463
frech <andreyka977@list.ru>
2021-09-14 01:54:07 +0300

    добавлена шейкерная сортировка

--- AlgorithmTests/CocktailSortTests.cs
+++ AlgorithmTests/CocktailSortTests.cs
@@ -9,31 +9,31 @@ using System.Threading.Tasks;
 namespace AlgorithmBase.Tests
 {
     [TestClass()]
-    public class BubbleSortTests
+    public class CocktailSortTests
     {
         [TestMethod()]
         public void SortTest()
         {
             //arrange
-            var bubble = new BubbleSort<int>();
+            var cocktail = new CocktailSort<int>();
             var rnd = new Random();
             var items = new List<int>();
 
-            for(int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-               items.Add(rnd.Next(0, 100));
+                items.Add(rnd.Next(0, 100));
             }
 
-            bubble.Items.AddRange(items);
+            cocktail.Items.AddRange(items);
             var sorted = items.OrderBy(x => x).ToArray();
 
             //act
-            bubble.Sort();
+            cocktail.Sort();
 
             //asser
-            for(int i = 0; i < items.Count; i++)
+            for (int i = 0; i < items.Count; i++)
             {
-                Assert.AreEqual(sorted[i], bubble.Items[i]);
+                Assert.AreEqual(sorted[i], cocktail.Items[i]);
             }
         }
     }
